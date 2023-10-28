1c8f856b939571f7bc9e7dcbdc4783caaf2c22a2
Abner Eslava <149258308+abnereslava@users.noreply.github.com>
2023-10-28 14:45:12 -0300

    Update 20231027.3-Lista-de-compras.py
    Added .lower() in line 7 to convert user input to lowercase

--- 20231027.3-Lista-de-compras.py
+++ 20231027.3-Lista-de-compras.py
@@ -4,12 +4,12 @@ positivo = ["sim", "s", "yes", "y"]
 negativo = ["não", "nao", "no", "n"]
 
 def inserirmaisitens():
-    resposta = input("Gostaria de inserir mais itens? [s/n]\n")
+    resposta = input("Gostaria de inserir mais itens? [s/n]\n").lower()
     if resposta in positivo:
-        return True #Essa linha retorna o valor "True" para a variável "caller" (linha 21), continuando o loop.
+        return True 
     elif resposta in negativo:
         print("Encerrando.")
-        return False #Essa linha retorna o valor "False" para a variável "caller" (linha 21), parando o loop.
+        return False 
     else:
         print("Resposta inválida. Tente novamente.")
         inserirmaisitens()
@@ -19,6 +19,6 @@ while continuar == True:
    novoitem = input("Insira o nome do item que precisa comprar: \n")
    listacompras.append(novoitem)
    print(f"\nItem acrescentado: {novoitem}. \nLista atual: {listacompras}.\n")
-   continuar = inserirmaisitens() #Esta variável é chamada de "caller", pois, ao mesmo tempo que chama a função, a armazena, podendo ter seu valor atualizado posteriormente para False ou True (linhas 9, 12 e 15), continuando ou parando o loop em conformidade com a condicional while (linha 17).
+   continuar = inserirmaisitens()
 
 print(f"Lista criada:\n{listacompras} \nBoas compras!")
